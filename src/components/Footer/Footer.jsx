79d4adecb1ba224ad9c10374b1d8619a21fd9d2c
OlenaShovkoplyas <olena.shovkoplyas@gmail.com>
2023-03-20 10:37:24 +0200

    add for now

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,32 +1,63 @@
 import React from "react";
-import './Footer.css'
+import './Footer.css';
+import TelegramIcon from '@mui/icons-material/Telegram';
+import FacebookOutlinedIcon from '@mui/icons-material/FacebookOutlined';
+import MailOutlineSharpIcon from '@mui/icons-material/MailOutlineSharp';
+import SupportAgentSharpIcon from '@mui/icons-material/SupportAgentSharp';
+import Diversity1SharpIcon from '@mui/icons-material/Diversity1Sharp';
+import RoofingSharpIcon from '@mui/icons-material/RoofingSharp';
+import LocalPoliceSharpIcon from '@mui/icons-material/LocalPoliceSharp';
+import ForwardToInboxSharpIcon from '@mui/icons-material/ForwardToInboxSharp';
+import logoman from '../../logoman.png'
+
 
 export default function Footer() {
   return ( 
-    <div className="footer">
-      <div className="d-flex">
-        <ul>
-          <li>
-            <a href="" className="about">About Us</a>
-          </li>
-          <li>
-            <a href="" className="privacy">Privacy Policy</a>
-          </li>
-          <li>
-            <a href="" className="email">Email: quiz-game@gmail.com</a>
-          </li>
-        </ul>
+    <div className="footer"> 
+      <div className="footer-box">        
+        <a href="" className="about">
+          <RoofingSharpIcon />
+          About Us
+        </a>
+        <a href="" className="privacy">
+          <LocalPoliceSharpIcon />
+          Privacy Policy
+        </a>          
+        <a href="" className="email">
+          <MailOutlineSharpIcon />
+          quiz-game@gmail.com
+        </a>          
       </div>
 
-      <div className="d-flex">
-        <ul>
-          <li>
-            <a href="" className="contacts">Contacts</a>
-          </li>
-          <li>
-            <a href="" className="public">Public</a>
-          </li>            
-        </ul>
+      <div className="footer-box">        
+        <a href="" >
+          <TelegramIcon />
+          Telegram
+        </a>
+        <a href="" >
+          <FacebookOutlinedIcon />
+          Facebook
+        </a>
+        <a href="" >
+          <ForwardToInboxSharpIcon />
+          Contact Us
+        </a>
+      </div>
+
+      <div className="footer-box">
+        <a href="" className="support">              
+          <SupportAgentSharpIcon />
+          Support
+        </a>
+        <a href="" className="public">
+          <Diversity1SharpIcon />
+          Public
+        </a>          
+      </div>
+
+      <div className="footer-box">
+        <img src={logoman} />
       </div> 
     </div>      
   )}
+  
