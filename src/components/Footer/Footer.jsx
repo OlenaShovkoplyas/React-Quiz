07fd76bdcb5b82d0e1e6525ca4d619d9b8b9d5bb
OlenaShovkoplyas <olena.shovkoplyas@gmail.com>
2023-03-20 08:15:38 +0200

    add footer base

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,7 +1,32 @@
 import React from "react";
+import './Footer.css'
 
 export default function Footer() {
-    return (       
-        <></>   
-      )
-}
+  return ( 
+    <div className="footer">
+      <div className="d-flex">
+        <ul>
+          <li>
+            <a href="" className="about">About Us</a>
+          </li>
+          <li>
+            <a href="" className="privacy">Privacy Policy</a>
+          </li>
+          <li>
+            <a href="" className="email">Email: quiz-game@gmail.com</a>
+          </li>
+        </ul>
+      </div>
+
+      <div className="d-flex">
+        <ul>
+          <li>
+            <a href="" className="contacts">Contacts</a>
+          </li>
+          <li>
+            <a href="" className="public">Public</a>
+          </li>            
+        </ul>
+      </div> 
+    </div>      
+  )}
