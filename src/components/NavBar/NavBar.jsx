f82670df76a35ff23492ec4a46d0f8cf1f6e9ea7
OlenaShovkoplyas <olena.shovkoplyas@gmail.com>
2023-03-21 13:06:50 +0200

    Merge branch 'main' of https://github.com/OlenaShovkoplyas/React-Quiz

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -1,36 +1,41 @@
 import React from "react";
+import { useState } from 'react';
+import { Button, Drawer, Box, } from '@mui/material';
+import MenuIcon from "@mui/icons-material/Menu";
+import NavList from "./NavList";
+import './NavBar.css';
 
-export default function NavBar() {
+export  default function NavBar ({ openDrawer }) {
+  const [open, setOpen] = useState(false);
+
   return (
-    <></>
+    <nav className="burgerBtn">
+      <Button 
+        onClick={() => setOpen(true)} 
+        sx={{color: 'white'}}
+      >
+      <MenuIcon />
+      </Button>
+      <Drawer 
+        anchor='left'
+        open={open}
+        onClose={() => setOpen(false)}
+      >
+        <Box
+          role="presentation"
+          onClick={() => setOpen(false)}
+          onKeyDown={() => setOpen(false)}
+          sx={{
+            height: '100%',
+            width: '250px',
+            backgroundColor: '#286b5598',
+            color: '#17342af2',
+            
+          }}
+        >
+            <NavList />
+        </Box>
+      </Drawer>
+    </nav>
   )
 }
-// import { Button, Drawer, Box, } from '@mui/material';
-// import MenuIcon from "@mui/icons-material/Menu";
-// import './NavBar.css';
-
-// export default function NavBar() {
-//   const [open, setOpen] = useState(false);
-
-//   return (
-//     <nav>
-//       <Button onClick={() => setOpen(true)}>
-//       <MenuIcon />
-//       </Button>
-//       <Drawer 
-//         anchor='right'
-//         open={open}
-//         onClose={() => setOpen(false)}
-//       >
-//         <Box         
-//           onClick={() => setOpen(false)}
-//           onKeyDown={() => setOpen(false)}
-//           sx={{
-//             height: '100%',            
-//           }}
-//         >            
-//         </Box>
-//       </Drawer>
-//     </nav>
-//   )
-// }
---
+++ src/components/NavBar/NavList.jsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import './NavBar.css';
+import {List, ListItem, ListItemButton, ListItemIcon, ListItemText} from "@mui/material";
+import RoofingSharpIcon from '@mui/icons-material/RoofingSharp';
+import CasinoIcon from '@mui/icons-material/Casino';
+import FavoriteBorderIcon from '@mui/icons-material/FavoriteBorder';
+import ConnectWithoutContactIcon from '@mui/icons-material/ConnectWithoutContact';
+
+export default function NavList() {
+  const pages = ['About Quiz', 'Quiz Challenge', 'Saved Quizes', 'Contacts'];
+  const icons = [<RoofingSharpIcon/>, <CasinoIcon/>, <FavoriteBorderIcon/>, <ConnectWithoutContactIcon/>];
+    
+  return (
+    <List className="burgerList">
+      {pages.map((page, index) => (
+        <ListItem key={page} disablePadding>
+          <ListItemButton>
+            <ListItemIcon sx={{color: 'white'}}>
+              {icons[index]}
+            </ListItemIcon>
+            <ListItemText primary={page}/>
+          </ListItemButton>
+        </ListItem>
+      ))}
+    </List>
+  )
+}
